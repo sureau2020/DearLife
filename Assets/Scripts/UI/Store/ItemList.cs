e2b3a8ae53ed36c714871c07e9f379ed87e5ed5f
sureau2020 <wen39717164@gmail.com>
2025-08-06 20:05:16 -0400

    check name exist when take item from database

--- Assets/Scripts/UI/Store/ItemList.cs
+++ Assets/Scripts/UI/Store/ItemList.cs
@@ -12,18 +12,25 @@ public class ItemList : MonoBehaviour
         GenerateAllGoodsByType(ItemType.Food);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    private void GenerateAllGoodsByType(ItemType type) { 
-        ItemDataBase.GetItemsByType(type).ForEach(item => {
+
+    private OperationResult GenerateAllGoodsByType(ItemType type)
+    {
+        var items = ItemDataBase.GetItemsByType(type);
+        foreach (var item in items)
+        {
             GameObject go = Instantiate(goodsPrefab, transform);
             Goods g = go.GetComponent<Goods>();
             g.SetItemData(item);
+
+            // 数据校验
+            if (item == null || string.IsNullOrEmpty(item.Name))
+            {
+                return OperationResult.Fail("疑似数据库商品数据有缺失，检查该类别未被加载出来的第一个商品数据。");
+            }
             g.ShowInfo();
-        });
+            
+        }
+        return OperationResult.Complete();
     }
 }
