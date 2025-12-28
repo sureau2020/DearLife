a21fde9fed0522af03eb69369e55ea9a431b5f30
sureau2020 <wen39717164@gmail.com>
2025-12-27 23:01:54 -0800

    Added abnormal state hint text， switch to mobile version

--- Assets/Scripts/Manager/BackgroundFXManager.cs
+++ Assets/Scripts/Manager/BackgroundFXManager.cs
@@ -1,9 +1,11 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class BackgroundFXManager : MonoBehaviour
 {
-    [SerializeField] private Image overlayImage; 
+    [SerializeField] private Image overlayImage;
+    [SerializeField] private TextMeshProUGUI statusText;
     private CharacterData character;
 
     private void Start()
@@ -25,22 +27,27 @@ public class BackgroundFXManager : MonoBehaviour
         {
             case HealthState.Normal:
                 SetOverlayColor(new Color(0, 0, 0, 0)); 
+                statusText.text = "";
                 break;
 
             case HealthState.Weak:
                 SetOverlayColor(new Color(0, 0, 1f, 0.25f)); // 蓝色半透明
+                statusText.text = "<wave>{characterName}<br>陷入了<br><color=#000000><shake>极度饥饿状态<@shake><@wave>";
                 break;
 
             case HealthState.Crazy:
                 SetOverlayColor(new Color(1f, 0, 0, 0.25f)); // 红色半透明
+                statusText.text = "<wave>{characterName}<br>陷入了<br><color=#FF0000><shake>疯狂状态<@shake><@wave>";
                 break;
 
             case HealthState.Dirty:
                 SetOverlayColor(new Color(0.5f, 0.25f, 0f, 0.2f)); // 棕色，表示脏
+                statusText.text = "<wave>{characterName}<br><color=#FFFFFF><shake>急需清洁<@shake><@wave>";
                 break;
 
             case HealthState.Dead:
                 SetOverlayColor(new Color(0, 0, 0, 0.7f)); // 黑屏
+                statusText.text = "";
                 break;
         }
     }
